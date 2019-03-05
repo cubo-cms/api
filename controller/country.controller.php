648a8226ccc4620bef4d3b209b598d61ae9bb7d2
papiando (HOME) <info@papiando.com>
2019-03-05 00:43:08 -0400

    Update country.controller.php

--- controller/country.controller.php
+++ controller/country.controller.php
@@ -1,9 +1,18 @@
 <?php
+/**
+ * @application    Cubo CMS API
+ * @type           Controller
+ * @class          CountryController
+ * @version        2.0.4
+ * @date           2019-03-05
+ * @author         Dan Barto
+ * @copyright      Copyright (c) 2019 Cubo CMS; see COPYRIGHT.md
+ * @license        MIT License; see LICENSE.md
+ */
 namespace Cubo;
-
-defined('__CUBO__') || new \Exception("No use starting a class without an include");
 
 class CountryController extends Controller {
 	protected $columns = ['name','title','nativename','alpha2','alpha3','accesslevel','status'];
+	protected $expandColumns = [];
 }
 ?>
